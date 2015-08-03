9a8e3534372e7caf505ae9830a6feb459d448509
Julie <julie.minjung.chung@gmail.com>
2015-08-03 17:35:49 -0400

    more buttons

--- code.js
+++ code.js
@@ -6,4 +6,33 @@ $( document ).ready(function() {
     $( "#continue" ).click(function() {
   		$("#firsttext").show();
 	});
+	$( "#dont" ).click(function() {
+  		$("#secondtext").show();
+	});
+	$( "#right" ).click(function() {
+  		$("#thirdtext").show();
+	});
+	$( "#do" ).click(function() {
+  		$("#fourthtext").show();
+	});
+	$( "#correct" ).click(function() {
+  		$("#fifthtext").show();
+	});
+	$( "#incorrect" ).click(function() {
+  		$("#sixthtext").show();
+	});
+	$( "#r" ).click(function() {
+  		$("#seventhtext").show();
+	});
+	$( "#w" ).click(function() {
+  		$("#eighthtext").show();
+	});
+	$( "#c" ).click(function() {
+  		$("#ninthtext").show();
+	});
+	$( "#i" ).click(function() {
+  		$("#tenthtext").show();
+	});
+
+
 });
