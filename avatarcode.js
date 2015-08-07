ae115b2872bf422ab36019e3f5c4d6fadd73c19d
Julie <julie.minjung.chung@gmail.com>
2015-08-07 18:57:04 -0400

    need help with changing avatar

--- avatarcode.js
+++ avatarcode.js
@@ -1,14 +1,14 @@
 $( document ).ready(function(){
 	$("#switch").click(function (){
 		$("#green").show();
+		$("#blue").hide();
+		$("#switch").click(function (){
+			$("#red").show();
+			$("#green").hide();
+		});
+		$("#switch").click(function (){
+			$("#yellow").show();
+			$("#red").hide();
+		});
 	});
-
-
-
-
-
-
-
-
-
 });
