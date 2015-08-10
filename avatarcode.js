3436a5a4685d59b145070032e4c5310db7970fd2
Julie <julie.minjung.chung@gmail.com>
2015-08-10 18:04:54 -0400

    choose gender, gender of that avatar shows. need help

--- avatarcode.js
+++ avatarcode.js
@@ -1,5 +1,8 @@
 var counter = 0;
 $( document ).ready(function(){
+	$( "#male" ).click(function() {
+  		$("#switch-guy-shirt").show();
+	});
 	$("#switch-guy-shirt").click(function (){
         $(".avatar-male-color").hide();
         if(counter == 0){
@@ -19,5 +22,27 @@ $( document ).ready(function(){
             counter = 0;
         }
 	});
-	
+	$( "#female" ).click(function() {
+  		$("#switch-girl-shirt").show();
+	});
+	$("#switch-girl-shirt").click(function (){
+        $(".avatar-male-color").hide();
+        if(counter == 0){
+            $("#green").show();
+		}
+		if(counter == 1){
+            $("#red").show();
+		}
+		else if(counter == 2){
+            $("#yellow").show();
+		}
+		else if(counter == 3){
+            $("#blue").show();
+		}
+        counter++;
+		if(counter == 4){
+            counter = 0;
+        }
+	});
+
 });
