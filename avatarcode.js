b1b97a8511d89fa9478c406d0fe824fdd086e998
Julie <julie.minjung.chung@gmail.com>
2015-08-13 20:32:24 -0400

    fixing avatar

--- avatarcode.js
+++ avatarcode.js
@@ -1,49 +1,51 @@
 var counter = 0;
 $( document ).ready(function(){
-	$( "#male" ).click(function() {
-  		$("#switch-guy-shirt").show();
-	});
-	$("#switch-guy-shirt").click(function (){
-        $(".avatar-male-color").hide();
-        if(counter == 0){
-            $("#green").show();
-		}
-		if(counter == 1){
-            $("#red").show();
-		}
-		else if(counter == 2){
-            $("#yellow").show();
-		}
-		else if(counter == 3){
-            $("#blue").show();
-		}
-        counter++;
-		if(counter == 4){
-            counter = 0;
-        }
-	});
-	$( "#female" ).click(function() {
-  		$("#switch-girl-shirt").show();
-	});
-	$("#switch-girl-shirt").click(function (){
-        $(".avatar-male-color").hide();
-        if(counter == 0){
-            $("#green").show();
-		}
-		if(counter == 1){
-            $("#red").show();
-		}
-		else if(counter == 2){
-            $("#yellow").show();
-		}
-		else if(counter == 3){
-            $("#blue").show();
-		}
-        counter++;
-		if(counter == 4){
-            counter = 0;
-        }
-	});
-
+	if(localStorage.getItem('avatar') === "guy"){
+		$(".guy").show();
+		$("#switch-guy-shirt").click(function (){
+			
+	        $(".avatar-male-color").hide();
+	        if(counter == 0){
+	            $(".green").show();
+			}
+			if(counter == 1){
+	            $(".red").show();
+			}
+			else if(counter == 2){
+	            $(".yellow").show();
+			}
+			else if(counter == 3){
+	            $(".blue").show();
+			}
+	        counter++;
+			if(counter == 4){
+	            counter = 0;
+	        }
+		});
+	}	
+	if(localStorage.getItem('avatar') === "girl"){
+		$(".girl").show();
+		$("#switch-girl-shirt").click(function (){
+	        $(".avatar-female-color").hide();
+	        if(counter == 0){
+	
+	            $(".girl .green").show();
+			}
+			if(counter == 1){
+				
+	            $(".red").show();
+			}
+			else if(counter == 2){
+	            $(".yellow").show();
+			}
+			else if(counter == 3){
+	            $(".blue").show();
+			}
+	        counter++;
+			if(counter == 4){
+	            counter = 0;
+	        }
+		});
+	}
 
 });
